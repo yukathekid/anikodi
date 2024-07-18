049d8dccf7b22451129b512d11061047b1910337
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-07-17 21:31:11 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ export default {
 
     // Verificar se a requisição é para o index.html
     if (url.pathname === '/api/v1/' || url.pathname === '/api/v1/index') {
-      const indexUrl = 'https://raw.githubusercontent.com/yukathekid/anikodi/main/api/v1/index';
+      const indexUrl = 'https://raw.githubusercontent.com/yukathekid/anikodi/main/api/v1/index.json';
       const response = await fetch(indexUrl);
       
       if (!response.ok) {
@@ -15,7 +15,7 @@ export default {
       }
       
       const headers = new Headers(response.headers);
-      headers.set('Content-Type', 'text/html');
+      headers.set('Content-Type', 'application/json');
       
       return new Response(response.body, {
         status: response.status,
