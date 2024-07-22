75b8d2af3de8e05e44c926c80e63b8f9087d249d
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-07-22 11:31:17 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,14 +2,24 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
+    // Rota para capturar o User-Agent
+    if (url.pathname === '/capture-user-agent') {
+      const userAgent1 = request.headers.get('User-Agent');
+      return new Response(`User-Agent: ${userAgent1}`, {
+        headers: { 'Content-Type': 'text/plain' }
+      });
+    }
+    
     // Verifica se a requisição é para o conteúdo do Pastebin
     if (url.pathname === '/paste') {
       const pastebinUrl = 'https://piroplay.xyz/cdn/hls/1f3225e82ea03a704c3a0f93272468d0/master.txt?s=4';
-      
+
       try {
+        // Faz a requisição ao Pastebin
         const response = await fetch(pastebinUrl);
         const content = await response.text();
-        
+
+        // Cria uma resposta com o conteúdo e força o download
         return new Response(content, {
           headers: {
             'Content-Type': 'text/plain',
@@ -28,12 +38,10 @@ export default {
       if (url.pathname === `/live/${jsonCategory}`) {
         const userAgent = request.headers.get('User-Agent');
         const isBrowser = userAgent && /Mozilla|Chrome|Safari|Firefox|Edge/i.test(userAgent);
-        const isKodi = userAgent && /Kodi\/16\.1/i.test(userAgent);
-        const isKodi21 = userAgent && /Kodi\/21\.0/i.test(userAgent);
-        const isPlaylistLoader = url.searchParams.get('source') === 'playlist_loader';
-
-        // Permite acesso apenas para Kodi, Playlist Loader com parâmetro específico ou outro User-Agent especificado
-        if (isBrowser || (!isKodi && !isKodi21 && !isPlaylistLoader)) {
+        const isKodi = userAgent && /Kodi\/\d+\.\d+/i.test(userAgent);
+        const isSpecificUserAgent = userAgent === 'Mozilla/5.0 (Linux; Android 13; M2103K19G Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.134 Mobile Safari/537.36';
+        // Permite acesso para Kodi e outros aplicativos de IPTV (ou por parâmetros, se necessário)
+        if (isBrowser && !isKodi && !isSpecificUserAgent) {
           return new Response('Access to this resource is restricted.', {
             status: 403,
             headers: {
@@ -80,14 +88,14 @@ export default {
     if (url.pathname === '/index.html') {
       const indexUrl = 'https://raw.githubusercontent.com/yukathekid/anikodi/main/index.html';
       const response = await fetch(indexUrl);
-      
+
       if (!response.ok) {
         return new Response('Index file not found', { status: 404 });
       }
-      
+
       const headers = new Headers(response.headers);
       headers.set('Content-Type', 'text/html');
-      
+
       return new Response(response.body, {
         status: response.status,
         headers: headers
@@ -99,7 +107,7 @@ export default {
       if (url.pathname.startsWith(`/${category}/`)) {
         const path = url.pathname.replace(`/${category}/`, '');
         const pathSegments = path.split('/');
-        
+
         const letter = pathSegments.shift();
         const imageId = pathSegments.pop();
         const categoryPath = pathSegments.join('/');
