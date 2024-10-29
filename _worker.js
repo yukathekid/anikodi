af58a052eae21015e130ae0c2a355a6166f922ee
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-10-29 12:40:52 -0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -19,10 +19,12 @@ export default {
       }
       
       // Verifica se o parâmetro `expire` da URL é menor ou igual ao `expireTimestamp` do Firestore
-      const isSessionExpired = expireParam === response.expiryDate && expireParam < Date.now();
-      if (isSessionExpired) {
-         return new Response('Sua sessão expirou. Por favor, renove o acesso.', { status: 403 });
-      }
+      // Verifica se o expireParam é igual ao expiryDate e se não está no passado
+const isSessionExpired = expireParam !== response.expiryDate || expireParam < Date.now();
+
+if (isSessionExpired) {
+  return new Response('Sua sessão expirou. Por favor, renove o acesso.', { status: 403 });
+}
 
       // Verifica o User-Agent após a autenticação bem-sucedida
       /*const userAgent = request.headers.get('User-Agent');
