c84894e90376d110a0f307e1d130f26c26d1db3b
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-10-30 13:22:21 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -35,7 +35,7 @@ function getM3UUrl(listType) {
     case 'animes':
       return 'https://vectorplayer.com/default.m3u'; // URL para a lista de animes
     case 'filmes':
-      return 'https://vectorplayer.com/filmes.m3u'; // URL para a lista de filmes
+      return 'https://raw.githubusercontent.com/JairPPereira/tvweb/refs/heads/main/jpiptv/lista2.m3u'; // URL para a lista de filmes
     // Adicione mais casos conforme necessário
     default:
       return 'https://vectorplayer.com/default.m3u'; // URL padrão
