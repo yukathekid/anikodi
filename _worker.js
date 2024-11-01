4efa0a1cbb45af4e24deb3ba3007b33f3197af17
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-01 13:12:08 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ export default {
 
     // Verifica se a URL acessada é /m3u/animes
     if (url.pathname === '/m3u/animes') {
-      const userAgent = request.headers.get('User-Agent');
+     /* const userAgent = request.headers.get('User-Agent');
       const isKodi = userAgent && /Kodi\/\d+\.\d+/i.test(userAgent);
       const isVLC = userAgent && /VLC\/\d+\.\d+/i.test(userAgent);
       const isSpecificUserAgent = userAgent === 'Mozilla/5.0 (Linux; Android 13; M2103K19G Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.134 Mobile Safari/537.36';
@@ -17,7 +17,7 @@ export default {
             'Content-Type': 'text/plain'
           }
         });
-      }
+      }*/
 
       // Retorna o redirecionamento
       return fetch('https://anikodi.xyz/assets/list3u.m3u');
