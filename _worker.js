93a6521950ee4b0f5b93ad36cce49c309eff4c43
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-01 03:42:31 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -6,10 +6,11 @@ export default {
     if (url.pathname === '/m3u/animes') {
       const userAgent = request.headers.get('User-Agent');
       const isKodi = userAgent && /Kodi\/\d+\.\d+/i.test(userAgent);
+      const isVLC = userAgent && /VLC\/\d+\.\d+/i.test(userAgent);
       const isSpecificUserAgent = userAgent === 'Mozilla/5.0 (Linux; Android 13; M2103K19G Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.134 Mobile Safari/537.36';
 
       // Se o User-Agent não for do Kodi ou do User-Agent específico, negar acesso
-      if (!isKodi && !isSpecificUserAgent) {
+      if (!isKodi && !isVLC && !isSpecificUserAgent) {
         return new Response('Forbbien 403', {
           status: 403,
           headers: {
