17561b97aa75d965a7ddb623f71762272705c21e
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-01 03:26:00 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -4,16 +4,10 @@ export default {
 
     // Verifica se a URL acessada é /m3u/animes
     if (url.pathname === '/m3u/animes') {
-      // URL de destino para redirecionamento
-      const targetUrl = 'https://vectorplayer.com/default.m3u';
-
       // Retorna o redirecionamento
       return fetch('https://vectorplayer.com/default.m3u');
     }
 
     return env.ASSETS.fetch(request);
-
-    // Retorna um 404 caso o caminho não corresponda ao desejado
-    return new Response('Not found', { status: 404 });
   }
 };
