aaab83dee1de4ab490a7acc30bbd871ebaabafea
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-06 07:39:30 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -64,7 +64,7 @@ export default {
     }
 
     // Verifica se a URL acessada é /m3u/filmes
-    if (url.pathname === '/ReiTv/playlist/filmes') {
+    if (url.pathname === '/playlist/filmes') {
       const firestoreUrl = 'https://firestore.googleapis.com/v1/projects/hwfilm23/databases/(default)/documents/users/filmes';
       const response = await fetch(firestoreUrl, {
         method: 'GET',
