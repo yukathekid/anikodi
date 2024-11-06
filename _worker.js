60204ea0924d3ea6d4c04c7dd632a55ddd71bcb5
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-06 12:03:02 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -86,7 +86,7 @@ export default {
         if (category === "expiryDate") continue;
         const rota = category === "Canais24h" ? "live" : "demand";
         const movies = data.fields.Teste.arrayValue.values;
-        if (const index in movies) {
+        for (const index in movies) {
           const movieF = movies[index].mapValue.fields;
           const title = movieF.title.stringValue;
           const logo = movieF.image.stringValue;
