3e04de73da6c7d841c4acfe477f43c0904034ff7
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-06 10:51:37 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -14,7 +14,6 @@ export default {
       const pathParts = url.pathname.split('/');
       const rots = pathParts[2];
       const name = pathParts[3];
-      const idVideo = parseInt(pathParts[4]);
      
       const urlAlt = 'https://api-f.streamable.com/api/v1/videos/qnyv36/mp4';
 
@@ -47,7 +46,7 @@ export default {
       for (const category in data.fields) {
         if (category === "expiryDate") continue; // Ignora o campo expiryDate
 
-        const movies = data.fields[category].mapValue.fields;
+        const movies = data.fields[category].arrayValue.values;
         if (movies[name]) {
          videoUrl = movies[name].mapValue.fields.url.stringValue;
          groupTitle = category; 
@@ -86,14 +85,14 @@ export default {
       for (const category in data.fields) {
         if (category === "expiryDate") continue;
         const rota = category === "Canais24h" ? "live" : "demand";
-        const movies = data.fields[category].mapValue.fields;
+        const movies = data.fields[category].arrayValue.values;
         for (const movieId in movies) {
           const movie = movies[movieId].mapValue.fields;
           const title = movie.title.stringValue;
           const logo = movie.image.stringValue;
-          const idVideo = movie.id.stringValue;
-          m3uList += `#EXTINF:-1 tvg-id="" tvg-name="${title}" tvg-logo="${logo}" group-title="${category}", ${title}\n`;
-          m3uList += `${url.origin}/ReiTv/${rota}/${movieId}/${idVideo}\n`;
+          const genero = movie.gender.stringValue;
+          m3uList += `#EXTINF:-1 tvg-id="" tvg-name="${title}" tvg-logo="${logo}" group-title="${genero}", ${title}\n`;
+          m3uList += `${url.origin}/ReiTv/${rota}/${movieId}\n`;
         }
       }
 
