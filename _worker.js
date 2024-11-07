8c89df9421f9f4442569b68efd325de463fad55a
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-07 00:47:22 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -91,7 +91,7 @@ export default {
           const title = movie.title.stringValue;
           const logo = movie.image.stringValue;
           m3uList += `#EXTINF:-1 tvg-id="" tvg-name="${title}" tvg-logo="${logo}" group-title="${category}", ${title}\n`;
-          m3uList += `${url.origin}/ReiTv/${rota}/${movieId}\n`;
+          m3uList += `${url.origin}/ReiTv/${rota}/${movie}\n`;
         }
       }
 
