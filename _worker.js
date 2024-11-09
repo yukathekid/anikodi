657e6f000c0e7616ab55b58071ada32095743d0b
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-09 02:12:12 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -1,22 +1,25 @@
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request) {
     const url = new URL(request.url);
     const userAgent = request.headers.get('User-Agent') || '';
 
-    // Simule os parâmetros do Xtream Codes
+    // Obter parâmetros do Xtream Codes
     const username = url.searchParams.get('username');
     const password = url.searchParams.get('password');
-    
-    // Verificação básica do User-Agent para permitir apenas o XCIPTV
+    const type = url.searchParams.get('type');
+    const output = url.searchParams.get('output');
+
+    // Verificar User-Agent para permitir apenas o XCIPTV
     if (!userAgent.includes('XCIPTV')) {
       return new Response('App não autorizado', { status: 403 });
     }
 
-    // Verifica se o username e password correspondem aos valores esperados
-    if (username === 'reitv' && password === 'OFFTV2424') {
+    // Verificar credenciais
+    if (username === 'reitv' && password === 'OFFTV2424' && type === 'm3u_plus' && output === 'ts') {
+      // Retornar a lista M3U
       return fetch('https://raw.githubusercontent.com/JairPPereira/tvweb/refs/heads/main/jpiptv/playtv.m3u');
     } else {
-      return new Response('Credenciais inválidas', { status: 403 });
+      return new Response('Credenciais inválidas ou parâmetros incorretos', { status: 403 });
     }
   }
 }
