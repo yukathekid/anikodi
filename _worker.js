8cef823f306d39a8c96ae9d25f63e25a27bf4971
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-13 04:54:00 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -66,9 +66,9 @@ export default {
     }
 
     // Verifica se a URL acessada é /playlist/filmes
-    const passHas = btoa(String(expireDate)).replace(/=+$/, '');
+    const pass = btoa(expireDate).replace(/=+$/, '');
     
-    if (pathParts[1] === 'reitv-vods' && pathParts[2] === passHas) {      
+    if (pathParts[1] === 'reitv-vods' && pathParts[2] === pass.toString()) {      
       const firestoreUrl = 'https://firestore.googleapis.com/v1/projects/hwfilm23/databases/(default)/documents/reitvbr/vods';
       const response = await fetch(firestoreUrl, {
         method: 'GET',
