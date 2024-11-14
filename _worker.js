e3fbf57eaf8729e2781cc09e8df77477638d5bb9
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-14 02:25:49 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -56,10 +56,10 @@ export default {
           message: userInfo.message ? userInfo.message.stringValue : null,
           auth: 1, // O valor para autenticação pode ser definido conforme necessário
           status: status,
-          exp_date: userInfo.exp_date ? new Date(userInfo.exp_date.timestampValue).getTime() : null,
+          exp_date: userInfo.exp_date ? userInfo.exp_date.timestampValue: null,
           is_trial: userInfo.is_trial ? userInfo.is_trial.stringValue : "0",
           active_cons: userInfo.active_cons ? userInfo.active_cons.stringValue : "0",
-          created_at: userInfo.created_at ? new Date(userInfo.created_at.timestampValue).getTime() : null,
+          created_at: userInfo.created_at ? userInfo.created_at.timestampValue : null,
           max_connections: userInfo.max_connections ? userInfo.max_connections.stringValue : "1",
           allowed_output_formats: userInfo.allowed_output_formats ? userInfo.allowed_output_formats.arrayValue.values.map(v => v.stringValue) : ["m3u8", "ts", "rtmp"]
         },
