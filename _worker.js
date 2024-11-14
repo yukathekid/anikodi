7d279049cf7bcefa621b4220c98010ca983968f9
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-14 02:29:57 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -56,10 +56,10 @@ export default {
           message: userInfo.message ? userInfo.message.stringValue : null,
           auth: 1, // O valor para autenticação pode ser definido conforme necessário
           status: status,
-          exp_date: userInfo.exp_date ? userInfo.exp_date.timestampValue : null,
+          exp_date: userInfo.exp_date ? new Date(userInfo.exp_date.timestampValue).getTime() : null,
           is_trial: userInfo.is_trial ? userInfo.is_trial.stringValue : "0",
           active_cons: userInfo.active_cons ? userInfo.active_cons.stringValue : "0",
-          created_at: userInfo.created_at ? userInfo.created_at.timestampValue : null,
+          created_at: userInfo.created_at ? new Data(userInfo.created_at.timestampValue).getTime() : null,
           max_connections: userInfo.max_connections ? userInfo.max_connections.stringValue : "1",
           allowed_output_formats: userInfo.allowed_output_formats ? userInfo.allowed_output_formats.arrayValue.values.map(v => v.stringValue) : ["m3u8", "ts", "rtmp"]
         },
@@ -70,7 +70,7 @@ export default {
           url: "anikodi.xyz",
           port: "80",
           https_port: "443",
-          server_protocol: "https",
+          server_protocol: "http",
           rtmp_port: "8880",
           timestamp_now: Date.now() / 1000, // Timestamp atual em segundos
           time_now: new Date().toISOString().slice(0, 19).replace('T', ' '), // Data atual formatada
