759a7470ba8745042d682ef4cb2c6ecd2b9716b4
Yukathekid <67480413+yukathekid@users.noreply.github.com>
2024-11-14 03:23:47 -0300

    Atualizar o _worker.js

--- _worker.js
+++ _worker.js
@@ -59,7 +59,7 @@ export default {
       // Se a URL do vídeo for encontrada, redireciona
       if (videoUrl) {
         return Response.redirect(videoUrl, 302);
-      } else(!videoUrl && expireDate < Date.now()){
+      } else if (!videoUrl && expireDate < Date.now()) {
         return Response.redirect(urlAlt, 302);
       }
     }
